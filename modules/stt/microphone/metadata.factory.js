7ca80d97c1b621fc66ce1484c79d55083eff9621
Jam <pjdsilvestre@tmj.ph>
2016-11-24 08:54:17 +0800

    added factory for metadata

---
+++ modules/stt/microphone/metadata.factory.js
@@ -0,0 +1,42 @@
+(function(){
+    'use strict';
+
+    angular
+        .module('microphone')
+        .factory('metadataFactory', metadataFactory);
+    metadataFactory.$inject = [];
+
+    function metadataFactory() {
+        var factory = {
+            INITIAL_OFFSET_X: 30;
+            INITIAL_OFFSET_Y: 30;
+            fontSize: 16;
+            delta_y: 2 * fontSize;
+            radius: 5;
+            space: 4;
+            hstep: 32;
+            timeout: 500;
+            defaultFont: fontSize + 'px Arial';
+            boldFont: 'bold ' + fontSize + 'px Arial';
+            italicFont: 'italic ' + fontSize + 'px Arial';
+            opacity: '0.6';
+
+            showAllHypotheses: true;
+            keywordsInputDirty: false;
+            keywords_to_search: [];
+            detected_keywords: {};
+            canvas: document.getElementById('canvas');
+            ctx: canvas.getContext('2d');
+            hslider: document.getElementById('hslider');
+            vslider: document.getElementById('vslider');
+            leftArrowEnabled: false;
+            rightArrowEnabled: false;
+            worker: null;
+            runTimer: false;
+            scrolled: false;
+            pushed: 0;
+            popped: 0;
+        }
+        return factory;
+    }
+})();
